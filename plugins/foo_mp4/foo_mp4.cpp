287b855fcdcc220d1c133562708c72864558257f
menno <menno>
2002-12-26 17:41:15 +0000

    no message

--- plugins/foo_mp4/foo_mp4.cpp
+++ plugins/foo_mp4/foo_mp4.cpp
@@ -1,3 +1,23 @@
+/*
+** FAAD - Freeware Advanced Audio Decoder
+** Copyright (C) 2002 M. Bakker
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**
+** $Id: foo_mp4.cpp,v 1.2 2002/12/26 17:41:15 menno Exp $
+**/
 
 #include <mp4.h>
 #include <faad.h>
@@ -8,7 +28,7 @@ class input_mp4 : public input
 {
 public:
 
-    virtual int test_filename(const WCHAR * fn,const WCHAR * ext) 
+    virtual int test_filename(const WCHAR * fn,const WCHAR * ext)
     {
         return !wcsicmp(ext,L"MP4");
     }
