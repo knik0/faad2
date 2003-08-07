9ebb1719095577af43e4cfcc36b76bd9c671afc4
menno <menno>
2003-08-07 17:21:21 +0000

    Added MPEG4IP plugin based on FAAD2 (by Bill May) Some modifications to makefiles (by Bill May)

--- include/faad.h
+++ include/faad.h
@@ -1,19 +1,19 @@
 /*
 ** FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
 ** Copyright (C) 2003 M. Bakker, Ahead Software AG, http://www.nero.com
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
 ** Any non-GPL usage of this software or parts of this software is strictly
@@ -22,7 +22,7 @@
 ** Commercial non-GPL licensing of this software is possible.
 ** For more info contact Ahead Software through Mpeg4AAClicense@nero.com.
 **
-** $Id: faad.h,v 1.26 2003/07/29 08:20:11 menno Exp $
+** $Id: faad.h,v 1.27 2003/08/07 17:21:21 menno Exp $
 **/
 
 #ifndef __AACDEC_H__
@@ -181,6 +181,8 @@ void* FAADAPI faacDecDecode(faacDecHandle hDecoder,
 char FAADAPI AudioSpecificConfig(unsigned char *pBuffer,
                                  unsigned long buffer_size,
                                  mp4AudioSpecificConfig *mp4ASC);
+
+void FAADAPI faacDecPostSeekReset(faacDecHandle hDecoder, unsigned long frame);
 
 #ifdef _WIN32
   #pragma pack(pop)
