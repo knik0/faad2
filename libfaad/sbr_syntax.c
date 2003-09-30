800f550eabc3155fcbae4c4b8dad3edcb61f3b5f
menno <menno>
2003-09-30 16:32:02 +0000

    1 more DRM fix

--- libfaad/sbr_syntax.c
+++ libfaad/sbr_syntax.c
@@ -1,19 +1,19 @@
 /*
 ** FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
 ** Copyright (C) 2003 M. Bakker, Ahead Software AG, http://www.nero.com
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
 ** Any non-GPL usage of this software or parts of this software is strictly
@@ -22,7 +22,7 @@
 ** Commercial non-GPL licensing of this software is possible.
 ** For more info contact Ahead Software through Mpeg4AAClicense@nero.com.
 **
-** $Id: sbr_syntax.c,v 1.10 2003/09/10 12:25:54 menno Exp $
+** $Id: sbr_syntax.c,v 1.11 2003/09/30 16:32:02 menno Exp $
 **/
 
 #include "common.h"
@@ -308,16 +308,16 @@ static uint8_t sbr_data(bitfile *ld, sbr_info *sbr, uint8_t id_aac)
     switch (id_aac)
     {
     case ID_SCE:
-		if ((result = sbr_single_channel_element(ld, sbr)) > 0)
-			return result;
+        if ((result = sbr_single_channel_element(ld, sbr)) > 0)
+            return result;
         break;
     case ID_CPE:
-		if ((result = sbr_channel_pair_element(ld, sbr)) > 0)
-			return result;
+        if ((result = sbr_channel_pair_element(ld, sbr)) > 0)
+            return result;
         break;
     }
 
-	return 0;
+    return 0;
 }
 
 /* table 5 */
@@ -608,7 +608,7 @@ static uint8_t sbr_grid(bitfile *ld, sbr_info *sbr, uint8_t ch)
             sbr->f[ch][env] = i;
 
         sbr->abs_bord_lead[ch] = 0;
-        sbr->abs_bord_trail[ch] = NO_TIME_SLOTS;
+        sbr->abs_bord_trail[ch] = sbr->numTimeSlots;
         sbr->n_rel_lead[ch] = bs_num_env - 1;
         sbr->n_rel_trail[ch] = 0;
         break;
@@ -680,7 +680,7 @@ static uint8_t sbr_grid(bitfile *ld, sbr_info *sbr, uint8_t ch)
         }
 
         sbr->abs_bord_lead[ch] = bs_abs_bord;
-        sbr->abs_bord_trail[ch] = NO_TIME_SLOTS;
+        sbr->abs_bord_trail[ch] = sbr->numTimeSlots;
         sbr->n_rel_lead[ch] = bs_num_env - 1;
         sbr->n_rel_trail[ch] = 0;
         break;
