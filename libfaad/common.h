03909f9041a382eeee5b6c5d3a9f89934a8b72e9
menno <menno>
2004-02-06 12:55:24 +0000

    more Final changes for version 2.0

--- libfaad/common.h
+++ libfaad/common.h
@@ -1,19 +1,19 @@
 /*
 ** FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
 ** Copyright (C) 2003-2004 M. Bakker, Ahead Software AG, http://www.nero.com
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
 ** Any non-GPL usage of this software or parts of this software is strictly
@@ -22,7 +22,7 @@
 ** Commercial non-GPL licensing of this software is possible.
 ** For more info contact Ahead Software through Mpeg4AAClicense@nero.com.
 **
-** $Id: common.h,v 1.49 2004/01/29 11:31:11 menno Exp $
+** $Id: common.h,v 1.50 2004/02/06 12:55:24 menno Exp $
 **/
 
 #ifndef __COMMON_H__
@@ -103,13 +103,16 @@ extern "C" {
 //#define SBR_LOW_POWER
 //#define PS_DEC
 
-/* FIXED POINT: No MAIN decoding, forced SBR Low Power decoder */
+/* FIXED POINT: No MAIN decoding, no SBR decoding */
 #ifdef FIXED_POINT
 # ifdef MAIN_DEC
 #  undef MAIN_DEC
 # endif
-# ifndef SBR_LOW_POWER
-#  define SBR_LOW_POWER
+//# ifndef SBR_LOW_POWER
+//#  define SBR_LOW_POWER
+//# endif
+# ifdef SBR_DEC
+#  undef SBR_DEC
 # endif
 #endif // FIXED_POINT
 
