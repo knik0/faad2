6c6392f7709f2c6de0764ee09e79c722570b3740
menno <menno>
2004-02-06 12:47:04 +0000

    more Final changes for version 2.0

--- common/mp4v2/mpeg4ip.h
+++ common/mp4v2/mpeg4ip.h
@@ -3,20 +3,20 @@
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is MPEG4IP.
- * 
+ *
  * The Initial Developer of the Original Code is Cisco Systems Inc.
  * Portions created by Cisco Systems Inc. are
  * Copyright (C) Cisco Systems Inc. 2000, 2001.  All Rights Reserved.
- * 
- * Contributor(s): 
- *		Dave Mackie		dmackie@cisco.com
+ *
+ * Contributor(s):
+ *      Dave Mackie     dmackie@cisco.com
  */
 
 #ifndef __MPEG4IP_INCLUDED__
@@ -24,6 +24,7 @@
 
 /* project wide applicable stuff here */
 
+#include <stdio.h>
 #include <systems.h>
 
 #ifndef FALSE
