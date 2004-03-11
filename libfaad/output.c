430570bcb5f7b3cd0d83c620fbeeb08bcda7fc6a
menno <menno>
2004-03-11 14:34:10 +0000

    typo fix

--- libfaad/output.c
+++ libfaad/output.c
@@ -1,19 +1,19 @@
 /*
 ** FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
 ** Copyright (C) 2003 M. Bakker, Ahead Software AG, http://www.nero.com
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
 ** Any non-GPL usage of this software or parts of this software is strictly
@@ -22,7 +22,7 @@
 ** Commercial non-GPL licensing of this software is possible.
 ** For more info contact Ahead Software through Mpeg4AAClicense@nero.com.
 **
-** $Id: output.c,v 1.37 2004/03/10 19:45:41 menno Exp $
+** $Id: output.c,v 1.38 2004/03/11 14:34:10 menno Exp $
 **/
 
 #include "common.h"
@@ -440,11 +440,11 @@ void *output_to_PCM(NeAACDecHandle hDecoder,
 #define RSQRT2 FRAC_CONST(0.7071067811865475244) // 1/sqrt(2)
 
 static INLINE real_t get_sample(real_t **input, uint8_t channel, uint16_t sample,
-                                uint8_t down_matrix, uint8_t down_matrix,
+                                uint8_t down_matrix, uint8_t up_matrix,
                                 uint8_t *internal_channel)
 {
     if (up_matrix == 1)
-        return  input[internal_channel[0]][sample];
+        return input[internal_channel[0]][sample];
 
     if (!down_matrix)
         return input[internal_channel[channel]][sample];
