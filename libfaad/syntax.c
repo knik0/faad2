c706456825317b743261b2bac90596fdd2d5e9b8
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-06-11 10:25:44 +0200

    libfaad: Fix a couple buffer overflows

--- libfaad/syntax.c
+++ libfaad/syntax.c
@@ -2304,6 +2304,8 @@ static uint8_t excluded_channels(bitfile *ld, drc_info *drc)
     while ((drc->additional_excluded_chns[n-1] = faad_get1bit(ld
         DEBUGVAR(1,104,"excluded_channels(): additional_excluded_chns"))) == 1)
     {
+        if (i >= MAX_CHANNELS - num_excl_chan - 7)
+            return n;
         for (i = num_excl_chan; i < num_excl_chan+7; i++)
         {
             drc->exclude_mask[i] = faad_get1bit(ld
