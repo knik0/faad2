knik0/faad2

commit 881b5e263f25a8a8b714e83407fc2c9dab407e40

Patch by Julian Cable: build the DRM version of the library as well as the normal version.

libfaad/Makefile.am
@@ -1,4 +1,4 @@
-lib_LTLIBRARIES = libfaad.la
+lib_LTLIBRARIES = libfaad.la libfaad_drm.la
 
 INCLUDES	= -iquote $(top_srcdir)/include
 include_HEADERS = $(top_srcdir)/include/faad.h \
@@ -29,3 +29,8 @@ libfaad_la_SOURCES = bits.c cfft.c decoder.c drc.c \
 		     codebook/hcb_1.h codebook/hcb_2.h codebook/hcb_3.h codebook/hcb_4.h \
 		     codebook/hcb_5.h codebook/hcb_6.h codebook/hcb_7.h codebook/hcb_8.h \
 		     codebook/hcb_9.h codebook/hcb_10.h codebook/hcb_11.h codebook/hcb_sf.h
+
+libfaad_drm_la_LDFLAGS = ${libfaad_la_LDFLAGS}
+libfaad_drm_la_LIBADD = ${libfaad_la_LIBADD}
+libfaad_drm_la_CFLAGS = ${libfaad_la_CFLAGS} -DDRM -DDRM_PS
+libfaad_drm_la_SOURCES = ${libfaad_la_SOURCES}

commit 8552fd53aaf5059ed769a0564330816beacaca98

fix compilation with LC_ONLY_DECODER
881b5e263f25a8a8b714e83407fc2c9dab407e40 broke compilation by always
defining DRN and DRM_PS. Fix this and place the define where it belongs.

libfaad/Makefile.am
@@ -32,5 +32,5 @@ libfaad_la_SOURCES = bits.c cfft.c decoder.c drc.c \
 
 libfaad_drm_la_LDFLAGS = ${libfaad_la_LDFLAGS}
 libfaad_drm_la_LIBADD = ${libfaad_la_LIBADD}
-libfaad_drm_la_CFLAGS = ${libfaad_la_CFLAGS} -DDRM -DDRM_PS
+libfaad_drm_la_CFLAGS = ${libfaad_la_CFLAGS} -DDRM_SUPPORT
 libfaad_drm_la_SOURCES = ${libfaad_la_SOURCES}

libfaad/common.h
@@ -91,8 +91,10 @@ extern "C" {
 /* Allow decoding of LD profile AAC */
 #define LD_DEC
 /* Allow decoding of Digital Radio Mondiale (DRM) */
-//#define DRM
-//#define DRM_PS
+#ifdef DRM_SUPPORT
+#define DRM
+#define DRM_PS
+#endif
 
 /* LD can't do without LTP */
 #ifdef LD_DEC
@@ -115,6 +117,7 @@ extern "C" {
   #undef MAIN_DEC
   #undef SSR_DEC
   #undef DRM
+  #undef DRM_PS
   #undef ALLOW_SMALL_FRAMELENGTH
   #undef ERROR_RESILIENCE
 #endif