072b7889bc5a2e8c18d4aa4dc4a30529b2d83488
Andrew Wesie <awesie@gmail.com>
2020-10-05 11:40:10 -0500

    Fix infinite loop in adts_parse.

--- frontend/main.c
+++ frontend/main.c
@@ -217,6 +217,8 @@ static int adts_parse(aac_buffer *b, int *bitrate, float *length)
 
             frame_length = ((((unsigned int)b->buffer[3] & 0x3)) << 11)
                 | (((unsigned int)b->buffer[4]) << 3) | (b->buffer[5] >> 5);
+            if (frame_length == 0)
+                break;
 
             t_framelength += frame_length;
 
