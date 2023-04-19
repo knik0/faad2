3bd54f37dbba00bd61d91496af28f6c433fadc25
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2023-04-19 16:56:35 +0000

    Improve decoder fuzzer
    Old code was passing same data to init and decode...
    
    Drive-by: add local fuzzer build script
    Drive-by: add license stubs

---
+++ fuzz/build_fuzz.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
+# Copyright (C) 2003-2005 M. Bakker, Nero AG, http://www.nero.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Any non-GPL usage of this software or parts of this software is strictly
+# forbidden.
+#
+# The "appropriate copyright message" mentioned in section 2c of the GPLv2
+# must read: "Code from FAAD2 is copyright (c) Nero AG, www.nero.com"
+#
+# Commercial non-GPL licensing of this software is possible.
+# For more info contact Nero AG through Mpeg4AAClicense@nero.com.
+
+export CC="clang"
+export CXX="clang++"
+export BASE_FLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link"
+export CFLAGS="${BASE_FLAGS}"
+export LDFLAGS="${BASE_FLAGS}"
+export CXXFLAGS="${BASE_FLAGS} -stdlib=libc++"
+
+./bootstrap
+./configure
+cd libfaad
+make -j `nproc`
+cd ../
+for fname in config decode; do
+  $CC $CFALGS -fsanitize=fuzzer,address -I./include ./fuzz/fuzz_${fname}.c -o ./fuzz/fuzz_${fname} ./libfaad/.libs/libfaad.a
+done
