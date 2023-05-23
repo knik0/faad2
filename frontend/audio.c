9b116bf62730c3d1f90134fadde8c4ff843a0c56
Eugene Kliuchnikov <eustas.ru@gmail.com>
2023-05-23 10:56:59 +0200

    Add FAAD CLI to CMake/Bazel
    Drive-by: remove automake/msvc build files

--- frontend/audio.c
+++ frontend/audio.c
@@ -41,6 +41,16 @@
 #include "unicode_support.h"
 #include "audio.h"
 
+static int write_wav_header(audio_file *aufile);
+static int write_wav_extensible_header(audio_file *aufile, long channelMask);
+static int write_audio_16bit(audio_file *aufile, void *sample_buffer,
+                             unsigned int samples);
+static int write_audio_24bit(audio_file *aufile, void *sample_buffer,
+                             unsigned int samples);
+static int write_audio_32bit(audio_file *aufile, void *sample_buffer,
+                             unsigned int samples);
+static int write_audio_float(audio_file *aufile, void *sample_buffer,
+                             unsigned int samples);
 
 audio_file *open_audio_file(char *infile, int samplerate, int channels,
                             int outputFormat, int fileType, long channelMask)
